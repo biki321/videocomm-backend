79ddefdc442507b517dc0d3acb96c11a6de4f3a6
biki321 <39851862+biki321@users.noreply.github.com>
2022-01-27 12:13:30 +0530

    cert added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: readFileSync('./privKey.pem'),
+    cert: readFileSync('./fullchain.pem'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   const config = app.get<ConfigService>(ConfigService);
 
   app.enableCors({
